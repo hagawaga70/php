90dc7881cebf55899cb430aea5c881cd7222632e
hagawaga <bjoern.woltering@gmx.de>
2018-05-17 00:15:23 +0200

    75%

--- index.php
+++ index.php
@@ -33,18 +33,28 @@
 
 			$db = pg_connect("host=localhost port=5432 dbname=fahraway user=parallels password=niewel");
 			/*$fahrtSequenceNr = pg_query($db,"SELECT nextval ('fahrtSeq'));*/
-			switch ($i) {
-				case "Apfel":
-					echo "i ist Apfel";
+			switch ($_GET['sort']) {
+				case "f_id":
+					$result = pg_query($db,"SELECT * FROM fahrt order by f_id");
 					break;
-				case "Balken":
-					echo "i ist Balken";
+				case "f_name":
+					$result = pg_query($db,"SELECT * FROM fahrt order by f_name");
 					break;
-				case "Kuchen":
-					echo "i ist Kuchen";
+				case "von":
+					$result = pg_query($db,"SELECT * FROM fahrt order by von");
 					break;
+				case "bis":
+					$result = pg_query($db,"SELECT * FROM fahrt order by bis");
+					break;
+				case "kl_ku":
+					$result = pg_query($db,"SELECT * FROM fahrt order by kl_ku");
+					break;
+				case "f_unterkunft":
+					$result = pg_query($db,"SELECT * FROM fahrt order by f_unterkunft");
+					break;
+				default:
+					$result = pg_query($db,"SELECT * FROM fahrt");
 			}
-			$result = pg_query($db,"SELECT * FROM fahrt");
 			echo "<table>";
 					echo "<tr>";
 						foreach ($spaltenBezeichnerFahrt as $key => $value)	{
