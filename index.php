7ceebfb0b39c27ac9666d8e905c56196d36eea82
hagawaga <bjoern.woltering@gmx.de>
2018-05-18 10:59:57 +0200

    Es geht voran:wq

--- index.php
+++ index.php
@@ -3,15 +3,57 @@
 	<head>
 		<meta charset="utf-8"/>	
 		<title>Fahraway</title>
-		<link rel="stylesheet" href="./html_2016.css">
+		<link rel="stylesheet" href="./fahraway.css">
 	</head>
 	<body>
-		<div id="rahmen_2">
-			<ul >
-				<li><a href="index.html">Fahrten</a></li>
-				<li><a href="marken.html">MARKEN</a></li>
-			</ul>	
+		<div id="navigation">
+			<div class="naviBoxSelect">
+  					<div class="naviInline">Fahrten:												</div>
+  					<div class="naviInline"><a class="ankerNavi"  href="index.php">Zeige</a>		</div>
+					<div class="naviInline"><a class="ankerNavi"  href="index.php">Suche</a>	    </div>
+			</div>
+			<div class="naviBox">
+  					<div class="naviInline">Schüler:												</div>
+  					<div class="naviInline"><a class="ankerNavi"  href="index.php">Zeige</a>		</div>
+					<div class="naviInline"><a class="ankerNavi"  href="index.php">Suche</a>	    </div>
+			</div>
+			<div class="naviBox">
+  					<div class="naviInline">Lehrer:													</div>
+  					<div class="naviInline"><a class="ankerNavi"  href="index.php">Zeige</a>		</div>
+					<div class="naviInline"><a class="ankerNavi"  href="index.php">Suche</a>	    </div>
+			</div>
+			<div class="naviBox">
+  					<div class="naviInline">Orte:													</div>
+  					<div class="naviInline"><a class="ankerNavi"  href="index.php">Zeige</a>		</div>
+					<div class="naviInline"><a class="ankerNavi"  href="index.php">Suche</a>	    </div>
+			</div>
+			<div class="naviBox">
+  					<div class="naviInline">Aktivitäten:											</div>
+  					<div class="naviInline"><a class="ankerNavi"  href="index.php">Zeige</a>		</div>
+					<div class="naviInline"><a class="ankerNavi"  href="index.php">Suche</a>	    </div>
+			</div>
+			<div class="naviBox">
+  					<div class="naviInline">Anbieter:												</div>
+  					<div class="naviInline"><a class="ankerNavi"  href="index.php">Zeige</a>		</div>
+					<div class="naviInline"><a class="ankerNavi"  href="index.php">Suche</a>	    </div>
+			</div>
+			<div class="naviBox">
+  					<div class="naviInline">Unterkünfte:											</div>
+  					<div class="naviInline"><a class="ankerNavi"  href="index.php">Zeige</a>		</div>
+					<div class="naviInline"><a class="ankerNavi"  href="index.php">Suche</a>	    </div>
+			</div>
+			<div class="naviBox">
+  					<div class="naviInline">Views 1:													</div>
+  					<div class="naviInline"><a class="ankerNavi"  href="index.php">Zeige</a>		</div>
+					<div class="naviInline"><a class="ankerNavi"  href="index.php">Suche</a>	    </div>
+			</div>
+			<div class="naviBox">
+  					<div class="naviInline">Views 2:													</div>
+  					<div class="naviInline"><a class="ankerNavi"  href="index.php">Zeige</a>		</div>
+					<div class="naviInline"><a class="ankerNavi"  href="index.php">Suche</a>	    </div>
+			</div>
 		</div>
+
 		<div id="rahmen_3">
 		<?php
 			$attributeFahrt = [
@@ -20,7 +62,7 @@
     			2	=> "von",
     			3	=> "bis",
     			4	=> "kl_ku",
-    			5	=> "f_unterkunft",
+    			5	=> "u_name",
 			];
 			$spaltenBezeichnerFahrt = [
     			"f_id" 			=> "Fahrt ID",
@@ -35,25 +77,66 @@
 			/*$fahrtSequenceNr = pg_query($db,"SELECT nextval ('fahrtSeq'));*/
 			switch ($_GET['sort']) {
 				case "f_id":
-					$result = pg_query($db,"SELECT * FROM fahrt order by f_id");
+					$result = pg_query($db,
+											"	SELECT 				f.f_id, f.f_name, f.von, f.bis,f.kl_ku, u.u_name 
+												FROM 				fahrt f 
+												LEFT OUTER JOIN 	unterkunft u 
+												ON 					(f.f_unterkunft=u.u_id)
+												ORDER BY			f.f_id;
+								");
 					break;
 				case "f_name":
-					$result = pg_query($db,"SELECT * FROM fahrt order by f_name");
+					$result = pg_query($db,
+											"	SELECT 				f.f_id, f.f_name, f.von, f.bis,f.kl_ku, u.u_name 
+												FROM 				fahrt f 
+												LEFT OUTER JOIN 	unterkunft u 
+												ON 					(f.f_unterkunft=u.u_id)
+												ORDER BY			f.f_name;
+								");
 					break;
 				case "von":
-					$result = pg_query($db,"SELECT * FROM fahrt order by von");
+					$result = pg_query($db,
+											"	SELECT 				f.f_id, f.f_name, f.von, f.bis,f.kl_ku, u.u_name 
+												FROM 				fahrt f 
+												LEFT OUTER JOIN 	unterkunft u 
+												ON 					(f.f_unterkunft=u.u_id)
+												ORDER BY			f.von;
+								");
 					break;
 				case "bis":
-					$result = pg_query($db,"SELECT * FROM fahrt order by bis");
+					$result = pg_query($db,
+											"	SELECT 				f.f_id, f.f_name, f.von, f.bis,f.kl_ku, u.u_name 
+												FROM 				fahrt f 
+												LEFT OUTER JOIN 	unterkunft u 
+												ON 					(f.f_unterkunft=u.u_id)
+												ORDER BY			f.bis;
+								");
 					break;
 				case "kl_ku":
-					$result = pg_query($db,"SELECT * FROM fahrt order by kl_ku");
+					$result = 	pg_query($db,
+											"	SELECT 				f.f_id, f.f_name, f.von, f.bis,f.kl_ku, u.u_name 
+												FROM 				fahrt f 
+												LEFT OUTER JOIN 	unterkunft u 
+												ON 					(f.f_unterkunft=u.u_id)
+												ORDER BY			f.kl_ku;
+								");
 					break;
 				case "f_unterkunft":
-					$result = pg_query($db,"SELECT * FROM fahrt order by f_unterkunft");
+					$result = 	pg_query($db,
+											"	SELECT 				f.f_id, f.f_name, f.von, f.bis,f.kl_ku, u.u_name 
+												FROM 				fahrt f 
+												LEFT OUTER JOIN 	unterkunft u 
+												ON 					(f.f_unterkunft=u.u_id)
+												ORDER BY			u.u_name;
+								");
 					break;
 				default:
-					$result = pg_query($db,"SELECT * FROM fahrt");
+					$result = pg_query($db,
+											"	SELECT 				f.f_id, f.f_name, f.von, f.bis,f.kl_ku, u.u_name 
+												FROM 				fahrt f 
+												LEFT OUTER JOIN 	unterkunft u 
+												ON 					(f.f_unterkunft=u.u_id)
+								");
 			}
 			echo "<table>";
 					echo "<tr>";
@@ -65,7 +148,11 @@
 				while($row=pg_fetch_assoc($result)){
 					echo "<tr>";
 						foreach ($attributeFahrt as $value)	{
-							echo "<td>" .$row[ $value] . "</td>";
+							if ($row[ $value ] == ''){
+								echo '<td class="rosa">' .$row[ $value] . "</td>";
+							}else{
+								echo "<td>" .$row[ $value] . "</td>";
+							}
 						}
 					echo "</tr>";
 				}
